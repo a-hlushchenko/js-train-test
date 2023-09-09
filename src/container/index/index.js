55e7360cc1c802fee8d830ca084fea16cd311e56
AntonHlushchenko <glantoshka@gmail.com>
2023-09-09 21:29:13 +0300

    init

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,36 @@
-import '../../script/test'
+document.querySelector('.button').onclick = () => {
+  const form = document.querySelector('.form')
 
-console.log('container')
+  if (!email.checkValidity()) {
+    if (email.value.length > 20) {
+      email.setCustomValidity(
+        'Значення має бути меньше 10 символів',
+      )
+    } else {
+      email.setCustomValidity('Введіть пошту')
+    }
+
+    email.reportValidity()
+  } else form.submit()
+}
+
+document.querySelectorAll('.change-age').forEach((el) => {
+  el.onclick = () => {
+    if (el.getAttribute('operator') === '+') {
+      age.stepUp(Number(el.getAttribute('value')))
+    } else if (el.getAttribute('operator') === '-') {
+      age.stepDown(Number(el.getAttribute('value')))
+    }
+  }
+})
+
+document.querySelector('.copy-button').onclick = () => {
+  const value = username.value
+
+  if (!value) {
+    alert('пусто')
+  } else {
+    navigator.clipboard.writeText(value)
+    alert('скопійовано')
+  }
+}
